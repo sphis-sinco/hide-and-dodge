118cec033c360f92af086a3d71ace7d295ae29b5
Viwu <sinconsistencia@gmail.com>
2025-10-22 15:48:24 -0500

    the time_starting_value and time_left data fields for time_decrease event packets are variables of the TimeDecreaseEventPacket class

--- source/sphis/hianod/events_packets/TimeDecreaseEventPacket.hx
+++ source/sphis/hianod/events_packets/TimeDecreaseEventPacket.hx
@@ -2,8 +2,14 @@ package sphis.hianod.events_packets;
 
 class TimeDecreaseEventPacket extends EventPacket
 {
+	public var time_starting_value:Int = 0;
+	public var time_left:Int = 0;
+
 	override public function new(time_starting_value:Int, time_left:Int)
 	{
-		super('time_decrease', {time_starting_value: time_starting_value, time_left: time_left});
+		this.time_starting_value = time_starting_value;
+		this.time_left = time_left;
+
+		super('time_decrease', {time_starting_value: this.time_starting_value, time_left: this.time_left});
 	}
 }
