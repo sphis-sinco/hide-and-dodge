39963aa6f2de04406d1c6a4de510df5c3648dfd4
Viwu <sinconsistencia@gmail.com>
2025-10-22 16:09:40 -0500

    InitState class

--- source/Main.hx
+++ source/Main.hx
@@ -8,6 +8,6 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, sphis.hianod.PlayState));
+		addChild(new FlxGame(0, 0, sphis.hianod.InitState));
 	}
 }
---
+++ source/sphis/hianod/InitState.hx
@@ -0,0 +1,14 @@
+package sphis.hianod;
+
+import flixel.FlxG;
+import flixel.FlxState;
+
+class InitState extends FlxState
+{
+	override function create()
+	{
+		super.create();
+
+		FlxG.switchState(() -> new PlayState());
+	}
+}
