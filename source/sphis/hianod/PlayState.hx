4ea00482066ee26e6bdf01a603207427582aa4c6
Viwu <sinconsistencia@gmail.com>
2025-10-22 15:29:15 -0500

    Added time_starting_value + time_decrease event dispatch

--- source/sphis/hianod/PlayState.hx
+++ source/sphis/hianod/PlayState.hx
@@ -5,17 +5,19 @@ import flixel.util.FlxTimer;
 
 class PlayState extends FlxState
 {
-	public var time_left:Int = 60;
+	public var time_starting_value:Int = 60;
+	public var time_left:Int = 0;
 
 	override public function create()
 	{
 		super.create();
 
+		time_left = time_starting_value;
 		new FlxTimer().start(1, function(timer:FlxTimer)
 		{
 			time_left -= 1;
 			dispatchEvent('time_decrease');
-		}, time_left);
+		}, time_starting_value);
 	}
 
 	override public function update(elapsed:Float)
@@ -32,6 +34,13 @@ class PlayState extends FlxState
 
 		switch (event_id)
 		{
+			case 'time_decrease':
+				var event_data = {
+					time_starting_value: this.time_starting_value,
+					time_left: this.time_left
+				};
+				trace(event_data);
+
 			default:
 				trace('Could not dispatch event: ' + event_id);
 				null_events.push(event_id);
