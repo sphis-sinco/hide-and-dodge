327e2df9ec7fcbcd7b985e7a3e94df52b6d5984b
Viwu <sinconsistencia@gmail.com>
2025-10-22 16:11:08 -0500

    register the GameInfo object

--- source/sphis/hianod/InitState.hx
+++ source/sphis/hianod/InitState.hx
@@ -2,12 +2,15 @@ package sphis.hianod;
 
 import flixel.FlxG;
 import flixel.FlxState;
+import flixel.system.debug.console.ConsoleUtil;
 
 class InitState extends FlxState
 {
 	override function create()
 	{
 		super.create();
+
+		ConsoleUtil.registerObject('GameInfo', GameInfo);
 
 		FlxG.switchState(() -> new PlayState());
 	}
