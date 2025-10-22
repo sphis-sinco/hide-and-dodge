434a873928fa552f75c6807a606d0baaf3f77cc2
Viwu <sinconsistencia@gmail.com>
2025-10-22 16:10:43 -0500

    public static vars

--- source/sphis/hianod/GameInfo.hx
+++ source/sphis/hianod/GameInfo.hx
@@ -4,12 +4,12 @@ import sphis.any.Version;
 
 class GameInfo
 {
-	public var game_version:Version = new Version(0, 1);
+	public static var game_version:Version = new Version(0, 1);
 
-	public var mod_api_version:Version = new Version(1, 0, 0);
-	public var mod_api_rule(get, never):String;
+	public static var mod_api_version:Version = new Version(1, 0, 0);
+	public static var mod_api_rule(get, never):String;
 
-	function get_mod_api_rule():String
+	static function get_mod_api_rule():String
 	{
 		final min_mod_api_version = new Version().fromVersion(mod_api_version, [1, 0, 0]).toString();
 		final max_mod_api_version = new Version().fromVersion(mod_api_version, [-1, 0, 0]).toString();
