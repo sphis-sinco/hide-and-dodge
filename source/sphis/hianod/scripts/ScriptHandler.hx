67793a3889dceff92b72c0c621e7b7fa34ed971f
Viwu <sinconsistencia@gmail.com>
2025-10-22 16:41:42 -0500

    scriptHandler

---
+++ source/sphis/hianod/scripts/ScriptHandler.hx
@@ -0,0 +1,14 @@
+package sphis.hianod.scripts;
+
+import sphis.hianod.events_packets.EventPacket;
+
+class ScriptHandler
+{
+	public static var scripts:Array<Script> = [];
+
+	public static function clearScripts() {}
+
+	public static function loadScripts() {}
+
+	public static function sendEventPacket(event_packet:EventPacket) {}
+}
